61750ed44c30490aa9dd91267597a8d541e917a2
ljbcalculations1 <ljb-mab@hotmail.co.uk>
2026-07-25 19:04:07 +0100

    Add monthly equity returns for the year 2000
    Updated the global equity returns dataset with monthly real returns for the year 2000.

--- global-equity-data.js
+++ global-equity-data.js
@@ -1,19 +1,69 @@
 // School Maths Investor
 //
-// Historical global equity investment journey data
-//
-// Methodology:
-// Global all-cap equity portfolio
-// Total returns including reinvested dividends
-// Monthly returns adjusted for inflation
-//
-// This dataset is used to demonstrate the effect
-// of regular investing over 10-year periods.
+// Test dataset:
+// Global equity monthly real returns
+// Inflation adjusted
 
 const globalEquityReturns = [
 
-  // =================================
-  // 1970s
-  // =================================
+{
+date:"2000-01",
+return:0.00998
+},
+
+{
+date:"2000-02",
+return:-0.03593
+},
+
+{
+date:"2000-03",
+return:-0.05988
+},
+
+{
+date:"2000-04",
+return:-0.02894
+},
+
+{
+date:"2000-05",
+return:0.02894
+},
+
+{
+date:"2000-06",
+return:0.01597
+},
+
+{
+date:"2000-07",
+return:-0.02295
+},
+
+{
+date:"2000-08",
+return:0.04291
+},
+
+{
+date:"2000-09",
+return:-0.06287
+},
+
+{
+date:"2000-10",
+return:-0.04092
+},
+
+{
+date:"2000-11",
+return:-0.02196
+},
+
+{
+date:"2000-12",
+return:0.02595
+}
 
 ];
