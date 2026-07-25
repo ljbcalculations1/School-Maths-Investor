612670c2af676bf0d1f90fa2bb41db9bc5bb0b7f
ljbcalculations1 <ljb-mab@hotmail.co.uk>
2026-07-25 18:20:43 +0100

    Update global-equity-data.js

--- global-equity-data.js
+++ global-equity-data.js
@@ -1,73 +1,14 @@
 // School Maths Investor
-// Historical monthly real returns
-// Diversified global equity portfolio
 //
-// Returns are decimal values:
-// 0.01 = +1%
-// -0.02 = -2%
-
+// Historical global equity investment journey data
+//
+// Returns are monthly real returns:
+// - adjusted for inflation
+// - including reinvested dividends
+//
+// This dataset is used to demonstrate the effect
+// of regular investing over 10-year periods.
 
 const globalEquityReturns = [
-
-  {
-date:"2000-01",
-return:0.012
-},
-
-{
-date:"2000-02",
-return:-0.034
-},
-
-{
-date:"2000-03",
-return:-0.058
-},
-
-{
-date:"2000-04",
-return:-0.027
-},
-
-{
-date:"2000-05",
-return:0.031
-},
-
-{
-date:"2000-06",
-return:0.018
-},
-
-{
-date:"2000-07",
-return:-0.021
-},
-
-{
-date:"2000-08",
-return:0.045
-},
-
-{
-date:"2000-09",
-return:-0.061
-},
-
-{
-date:"2000-10",
-return:-0.039
-},
-
-{
-date:"2000-11",
-return:-0.020
-},
-
-{
-date:"2000-12",
-return:0.028
-}
-
 
 ];
